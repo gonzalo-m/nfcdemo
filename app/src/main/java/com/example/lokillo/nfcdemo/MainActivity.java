b5e84e9b01c1d0052e8d3fc4265fcc3dddd85223
Gonzalo Martinez <gmartz21@gmail.com>
2015-06-10 22:33:23 -0400

    minor changes

--- app/src/main/java/com/example/lokillo/nfcdemo/MainActivity.java
+++ app/src/main/java/com/example/lokillo/nfcdemo/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.lokillo.nfcdemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -13,10 +18,20 @@ public class MainActivity extends Activity {
 
     public static String TAG = "NFC";
 
+    private TextView button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (TextView)findViewById(R.id.click_text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "text clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         // check if device supports NFC
         if (isNFCSupported()) {
@@ -36,12 +51,21 @@ public class MainActivity extends Activity {
         // add NFC setting to UI
 
         // ask user to enable NFC, if not enabled
+        NfcAdapter nfcAdapt = NfcAdapter.getDefaultAdapter(getApplicationContext());
+        if(!nfcAdapt.isEnabled()) {
+            startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+        }
+
+        Toast.makeText(this, nfcAdapt.toString(), Toast.LENGTH_SHORT).show();
 
     }
 
     private void stopNFC() {
         Log.i(TAG, "NFC not supported");
         Toast.makeText(this, "NFC is not supported", Toast.LENGTH_SHORT).show();
+        NfcAdapter nfcAdapt = NfcAdapter.getDefaultAdapter(getApplicationContext());
+        if (nfcAdapt != null)
+            Toast.makeText(this, nfcAdapt.toString(), Toast.LENGTH_SHORT).show();
 
     }
 
